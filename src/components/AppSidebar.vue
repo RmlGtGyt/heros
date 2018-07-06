40bb5fe0f27cf8b1d2caf9fbdebba04d8823bd54
RmlGtGyt <781273753@qq.com>
2018-07-06 11:46:11 +0800

    初始化文件、首页头部和侧边栏导出和引入

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-sm-3 col-md-2 sidebar">
+    <ul class="nav nav-sidebar">
+      <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+      <li><a href="#">Reports</a></li>
+      <li><a href="#">Analytics</a></li>
+      <li><a href="#">Export</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  // 导出侧边栏
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  };
+</script>
+
+<style lang="">
+  
+</style>
