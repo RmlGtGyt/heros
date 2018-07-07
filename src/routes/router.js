33139f412f4210160e365680f20f0ac15d0bde9f
RmlGtGyt <781273753@qq.com>
2018-07-07 12:04:47 +0800

    配置路由和展示列表

---
+++ src/routes/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+// 1. 导入vue-router模块
+import VueRouter from 'vue-router';
+
+// 导入组件
+import HerList from '../views/heroes/List.vue';
+import WeaponList from '../views/weapons/List.vue';
+import EquipList from '../views/equips/List.vue';
+
+// 注册插件
+Vue.use(VueRouter);
+
+// 2. 创建路由对象，配置路由规则
+const router = new VueRouter({
+  linkExactActiveClass: 'active',
+  routes: [
+    {name: 'home', path: '/', redirect: {name: 'heroes'}},
+    // 路由规则
+    { name: 'heroes', path: '/heroes', component: HerList},
+    { name: 'weapons', path: '/weapons', component: WeaponList},
+    { name: 'equips', path: '/equips', component: EquipList},
+  ]
+});
+// 3. 导出模块
+export default router;
